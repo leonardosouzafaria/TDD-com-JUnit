03ace713ebe921407185333598f45c2a502a36b5
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2021-12-12 19:02:26 -0300

    Refatorando class de teste

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -6,24 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
 
 class BonusServiceTest {
 
+	private BonusService service;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void inicializar() {
+		this.service = new BonusService();
+	}
+
 	@Test
 	void lançaIllegalArgumentExceptionParaFuncionarioComSalarioMuitoAlto() {
-		BonusService service = new BonusService();
 		assertThrows(IllegalArgumentException.class, 
 				()-> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
 	}
 
 	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
-		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
-
 		assertEquals(new BigDecimal("250.00"), bonus);
 	}
 
